febc186bbff0721c5d9217812a5b1929af6efce6
Jiazz-J <krishnakishore.1310@gmail.com>
2021-11-17 23:39:18 +0530

    added extractMax for heap

--- src/heaps/max-binary-heap.ts
+++ src/heaps/max-binary-heap.ts
@@ -24,6 +24,35 @@ class MaxBinaryHeap {
       this.values[index],
     ];
   }
+
+  //extract max
+  /// children of a node - 2n+1, 2n+2
+  extractMax() {
+    if (this.values.length > 0) {
+      this.swap(0, this.values.length - 1);
+      const maxValue = this.values.pop();
+      this.bubbleDown(0);
+      return maxValue;
+    }
+    return null;
+  }
+
+  private bubbleDown(index: number) {
+    if (index < this.values.length) {
+      const childIndex = 2 * index; // Out of bound check should be performed
+      const child_1 = this.values[childIndex + 1];
+      const child_2 = this.values[childIndex + 2];
+      const maxElement = Math.max(child_1, child_2);
+      if (maxElement > this.values[index]) {
+        const indexToBeMoved =
+          maxElement === child_1 ? childIndex + 1 : childIndex + 2;
+        this.swap(index, indexToBeMoved);
+        this.bubbleDown(indexToBeMoved);
+      } else {
+        return;
+      }
+    }
+  }
 }
 
 const maxHeap = new MaxBinaryHeap();
@@ -34,5 +63,9 @@ maxHeap.insert(18);
 maxHeap.insert(27);
 maxHeap.insert(12);
 maxHeap.insert(55);
+
+console.log(maxHeap.values);
+
+maxHeap.extractMax();
 
 console.log(maxHeap.values);
