7e66954db1f4e5831b447f3d6e0b825a8745cf23
Jiazz-J <krishnakishore.1310@gmail.com>
2021-11-24 23:12:19 +0530

    added graph traversals

--- src/graphs/graphs-adjacency-list.js
+++ src/graphs/graphs-adjacency-list.js
@@ -37,9 +37,93 @@ class Graph {
     this.adjacencyList.get(vertex).forEach(ele => this.removeEdge(vertex, ele));
     this.adjacencyList.delete(vertex);
   }
+
+  dfsRecursive(vertex) {
+    const result = [];
+
+    this.dfs(vertex, result);
+
+    return result;
+  }
+
+  dfs(vertex, result) {
+    if (!vertex) {
+      return;
+    }
+
+    result.push(vertex);
+
+    for (const neighbourVertex of this.adjacencyList.get(vertex)) {
+      if (!result.includes(neighbourVertex)) {
+        this.dfs(neighbourVertex, result);
+      }
+    }
+  }
+
+  // Could have used extra data structure map to store visisted nodes and gives constant lookups
+  dfsIterative(start) {
+    const stack = [];
+    const result = [];
+    stack.push(start);
+
+    while (stack.length) {
+      const vertex = stack.pop();
+      if (!result.includes(vertex)) {
+        result.push(vertex);
+
+        for (const neighbour of this.adjacencyList.get(vertex)) {
+          stack.push(neighbour);
+        }
+      }
+    }
+
+    return result;
+  }
+
+  bfsIterative(start) {
+    const queue = [];
+    const result = [];
+    queue.push(start);
+
+    while (queue.length) {
+      const vertex = queue.shift();
+
+      if (!result.includes(vertex)) {
+        result.push(vertex);
+
+        for (const neighbour of this.adjacencyList.get(vertex)) {
+          queue.push(neighbour);
+        }
+      }
+    }
+
+    return result;
+  }
 }
 
 const g = new Graph();
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+
+console.log(g.dfsRecursive('A'));
+
+console.log(g.dfsIterative('A'));
+
+console.log(g.bfsIterative('A'));
+
+/* const g = new Graph();
 g.addVertex('Tokyo');
 g.addVertex('Delhi');
 console.log(g.adjacencyList);
@@ -48,3 +132,4 @@ console.log(g.adjacencyList);
 //g.removeEdge('Tokyo', 'Delhi');
 g.removeVertex('Delhi');
 console.log(g.adjacencyList);
+ */
